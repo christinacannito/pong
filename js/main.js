502af820a6b20b2598ebce5e53011faeab07156e
christinacannito <christinacannito@gmail.com>
2016-04-04 21:47:06 -0400

    almost made the animation part

--- js/main.js
+++ js/main.js
@@ -10,26 +10,47 @@ $(document).ready(function(){
 	line.stroke(); // draw the line
 
 	// the prototype for the paddles
-	function Paddle(xpos, ypos, pwidth, pheight) {
-		this.xpos = xpos;
-		this.ypos = ypos;
-		this.pwidth = pwidth;
-		this.pheight = pheight;
+	function Paddle(speed) {
+		// this.xpos = xpos;
+		// this.ypos = ypos;
+		// this.pwidth = pwidth;
+		// this.pheight = pheight;
 		// this.pcolor = pcolor;
-		this.render = function () {
+		this.speed= speed;
+		this.render = function (x, y, pwidth, pheight) {
 			var paddle = table[0].getContext('2d');
-			paddle.rect(this.xpos, this.ypos, pwidth, pheight); // x, y, width, height
+			paddle.rect(x, y, pwidth, pheight); // x, y, width, height
 			paddle.stroke();
+		}
+		this.move = function(speed, pheight) {
+			console.log('inside move')
+			console.log('speed: ', speed)
+			$(window).keydown(function(e){
+				console.log('inside keydown')
+				console.log('e: ', e.which)
+				if ( e.which == 38 ) {
+					console.log('up was pressed');
+					// you will want redraw the paddle up 6pixels every time the up arrow is pressed 
+					pheight = pheight + speed; 
+					console.log('pheight HERE: ', pheight);
+					requestAnimationFrame(animate(pheight)); // then say to animate the paddle
+				} else if ( e.which == 40 ) {
+					console.log('down was pressed');
+					// will want to move the paddle down 6 pixels every time the down arrow is pressed
+					pheight = pheight - speed;
+					requestAnimationFrame(animate(pheight)); // then say to animate the paddle
+				}
+			})			
 		}
 	} // paddle object
 
 	// might change the parameters here
 	function Player (paddle) {
-		this.paddle = new Paddle(0, 200, 20, 20, "#F5A999");
+		this.paddle = new Paddle(0, 200, 20, 20);
 	} // player object
 
 	function Computer (paddle) {
-		this.paddle = new Paddle(200, 800, 20, 20, "#D3D4C4");
+		this.paddle = new Paddle(200, 800, 20, 20);
 	} // computer object
 
 	function Ball (ballx, bally) {
@@ -44,12 +65,27 @@ $(document).ready(function(){
 		}
 	}
 
+	// animating the objects
+	var animate = function (step) {
+		// console.log('step: ', step)
+		// requestAnimationFrame(animate);
+		// animate something
+		// console.log('inside animate function')
+
+		// then you have to change the paddle coordinates
+		leftPaddle.render(0, pheight, 50, 100);
+	}
+
+	// requestAnimationFrame(animate); // will get called from the move function
+
+	// drawing, creating the objects
 	var firstBall = new Ball(200, 100);
 	firstBall.render(); // draws the ball
 
-	var leftPaddle = new Paddle(0, 100, 50, 100);
-	leftPaddle.render();
+	var leftPaddle = new Paddle(6);
+	leftPaddle.render(0, 100, 50, 100);
+	leftPaddle.move(6, 100);
 
-	var rightPaddle = new Paddle(750, 100, 50, 100);
-	rightPaddle.render();
+	var rightPaddle = new Paddle(6);
+	rightPaddle.render(750, 100, 50, 100);
 });
